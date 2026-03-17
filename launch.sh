3a29048a7ca26892788f5c66d9d177632586f16c
David Cordeiro <up202108820@g.uporto.pt>
2026-03-17 21:09:42 +0000

    Increase the batch_size

--- launch.sh
+++ launch.sh
@@ -8,4 +8,4 @@
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python train.py --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg" --run_name "avg_flowers_lpipsSorted" --partition_file "partition.csv" --batch_size 128 --epochs 150
+python train.py --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg" --run_name "avg_flowers_lpipsSorted" --partition_file "partition.csv" --batch_size 192 --epochs 150
