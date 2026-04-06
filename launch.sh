84668fae9334b6522b729cfb5783a17eaf28d343
David Amorim Cordeiro <dacordeiro@ctm-login-01.inesctec.pt>
2026-04-06 12:16:24 +0100

    Finish tests

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min12gb_ext            # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min12gb_ext                  # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
-#SBATCH --job-name=lt_avg_flower_flower_mean_test        # Job name
+#SBATCH --partition=gpu_min11gb_ext            # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min11gb_ext                  # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --job-name=lt_avg_flower_flower_mean        # Job name
 #SBATCH --output=slurm_%x.%j.out               # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err                # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python train.py --data_dir "data_embeddings_same_dataset" --image_dir1 "/nas-ctm01/datasets/public/Oxford102Flowers/jpg/" --image_dir2 "/nas-ctm01/datasets/public/Oxford102Flowers/jpg/" --run_name "lt_avg_flower_flower_mean_test" --batch_size 768 --epochs 30
+python train.py --data_dir "data_embeddings_same_dataset" --image_dir1 "/nas-ctm01/datasets/public/Oxford102Flowers/jpg/" --image_dir2 "/nas-ctm01/datasets/public/Oxford102Flowers/jpg/" --run_name "lt_avg_flower_flower_mean" --batch_size 768 --epochs 30
