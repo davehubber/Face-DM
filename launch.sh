aeca9c0ed77ba1351054a6229187b751991baaf0
David Amorim Cordeiro <dacordeiro@ctm-login-02.inesctec.pt>
2026-04-21 16:09:34 +0100

    Save magnitude experiments

--- launch.sh
+++ launch.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --partition=gpu_min8gb_ext            # Partition where the job will be run. Check with "$ sinfo".
 #SBATCH --qos=gpu_min8gb_ext                  # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
-#SBATCH --job-name=lt_avg_mag_5ts              # Job name
+#SBATCH --job-name=lt_avg_mag                 # Job name
 #SBATCH --output=slurm_%x.%j.out              # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err               # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python train_semantic.py --run_name "lt_avg_mag_5ts" --epochs 10 --batch_size 8192 --max_timesteps 5 --mixed_precision "no"
+python train_semantic.py --run_name "lt_avg_mag" --epochs 10 --batch_size 8192 --mixed_precision "no" --injected_mse 0.5 --simulated_sampling_steps 5
