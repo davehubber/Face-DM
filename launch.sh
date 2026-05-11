bcf53a7d74769a227c193a41dbe0933a22dd2fdc
David Amorim Cordeiro <dacordeiro@ctm-login-02.inesctec.pt>
2026-05-11 09:26:44 +0100

    Test one time but delete bad results

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min24gb_ext                     # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min24gb_ext                           # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
-#SBATCH --job-name=avg_diffae_pit_l1EnergyPen_10ts_constLR       # Job name
-#SBATCH --output=slurm_%x.%j.out                        # File containing STDOUT output
-#SBATCH --error=slurm_%x.%j.err                         # File containing STDERR output. If ommited, use STDOUT.
+#SBATCH --partition=gpu_min32gb_ext                        # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min32gb_ext                              # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --job-name=avg_diffae_pit_l1EnergyPen0.01_10ts_constLR # Job name
+#SBATCH --output=slurm_%x.%j.out                           # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err                            # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python train_latent.py --run_name "avg_diffae_pit_l1EnergyPen_10ts_constLR" --max_timesteps 10 --batch_size 24576 --epochs 15
+python train_latent.py --run_name "avg_diffae_pit_l1EnergyPen0.01_10ts_constLR" --max_timesteps 10 --batch_size 24576 --epochs 15 --energy_loss_weight 0.01
