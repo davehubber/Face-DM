90f14e35fbf07fe2deddf5aa5aa8ae21808f93f4
David Amorim Cordeiro <dacordeiro@ctm-login-01.inesctec.pt>
2026-05-13 08:26:08 +0100

    Finish training

--- launch.sh
+++ launch.sh
@@ -2,17 +2,19 @@
 #
 #SBATCH --partition=gpu_min8gb_ext                         # Partition where the job will be run. Check with "$ sinfo".
 #SBATCH --qos=gpu_min8gb_ext                               # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
-#SBATCH --job-name=encode_diffae_celeba                           # Job name
+#SBATCH --job-name=train_celeba_mlp                           # Job name
 #SBATCH --output=slurm_%x.%j.out                            # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err                             # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python encode_celeba_diffae_for_identity_mlp.py \
-  --diffae-root /nas-ctm01/homes/dacordeiro/diffae \
-  --checkpoint /nas-ctm01/homes/dacordeiro/Face-DM/ffhq256_autoenc/last.ckpt \
-  --image-root /nas-ctm01/datasets/public/celeba \
-  --identity-file /nas-ctm01/homes/dacordeiro/Face-DM/identity_CelebA_png.txt \
-  --out-dir /nas-ctm01/homes/dacordeiro/Face-DM/diffae_embeddings_celeba \
-  --min-images-per-identity 30 \
-  --batch-size 1
+python train_celeba_mlp.py \
+  --dataset-dir /nas-ctm01/homes/dacordeiro/Face-DM/diffae_embeddings_celeba \
+  --out-dir /nas-ctm01/homes/dacordeiro/Face-DM/experiments/celeba_200id_diffae_identity_mlp \
+  --epochs 100 \
+  --batch-size 256 \
+  --hidden-dim 512 \
+  --dropout 0.20 \
+  --lr 1e-3 \
+  --weight-decay 1e-4 \
+  --patience 20
