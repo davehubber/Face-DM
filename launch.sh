c3b475a75fd128cd4cf6827f763bc20870a680ce
David Amorim Cordeiro <dacordeiro@ctm-login-03.inesctec.pt>
2026-05-14 11:20:14 +0100

    Save test result

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min8gb_ext                         # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min8gb_ext                               # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min11gb_ext                         # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min11gb_ext                               # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=decode_celeba_test                          # Job name
 #SBATCH --output=slurm_%x.%j.out                            # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err                             # File containing STDERR output. If ommited, use STDOUT.
@@ -12,8 +12,8 @@ python decode_celeba_test.py \
   --diffae-root /nas-ctm01/homes/dacordeiro/diffae \
   --checkpoint /nas-ctm01/homes/dacordeiro/Face-DM/ffhq256_autoenc/last.ckpt \
   --dataset-dir /nas-ctm01/homes/dacordeiro/Face-DM/diffae_embeddings_celeba \
-  --embeddings-file diffae_embeddings_celeba/celeba_diffae_zsem.npy \
-  --metadata-file diffae_embeddings_celeba/celeba_diffae_zsem_metadata.csv \
+  --embeddings-file celeba_diffae_zsem.npy \
+  --metadata-file celeba_diffae_zsem_metadata.csv \
   --index 0 \
   --out-dir /nas-ctm01/homes/dacordeiro/Face-DM/debug_decode_celeba_zsem \
   --t-inv 200 \
